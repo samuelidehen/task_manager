9481da8a9b8492be292715e9bff18624fad506d5
Samuel Idehen <samuelidehenlion@gmail.com>
2020-04-12 16:44:29 -0400

    Updates

---
+++ precache-manifest.f8196b380805c0d971dfef4da2d9b973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f85d7f7a52fb63a130169742aee523e",
+    "url": "/task_manager/index.html"
+  },
+  {
+    "revision": "8f0764b9599e0e04dcaa",
+    "url": "/task_manager/static/css/main.9954d55d.chunk.css"
+  },
+  {
+    "revision": "4f67a0d34b538be7b709",
+    "url": "/task_manager/static/js/2.b303b527.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task_manager/static/js/2.b303b527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0764b9599e0e04dcaa",
+    "url": "/task_manager/static/js/main.3642f5aa.chunk.js"
+  },
+  {
+    "revision": "af943ac97cc54bb43f95",
+    "url": "/task_manager/static/js/runtime-main.2f96c515.js"
+  }
+]);
